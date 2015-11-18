11d7d4dc4c9b0fceccf83e82a5a4c076c8a4b3b3
Karolis Astrauka <astrauka@gmail.com>
2015-11-18 23:46:55 +0200

    Update lein profile

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,5 +1,4 @@
-{:user {
- :plugins [[cider/cider-nrepl "0.9.1"]
-           [refactor-nrepl "1.1.0"]]
- :dependencies [[org.clojure/tools.nrepl "0.2.7"]]
- }}
+{:user {:plugins      [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
+                       [refactor-nrepl "2.0.0-SNAPSHOT"]]
+        :dependencies [[alembic "0.3.2"]
+                       [org.clojure/tools.nrepl "0.2.12"]]}}
