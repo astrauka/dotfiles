44e4932ad6a3da8027c9e453b61d59104d10c515
Karolis Astrauka <astrauka@gmail.com>
2016-02-27 15:42:07 +0200

    Update lein dependencies

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,4 @@
-{:user {:plugins      [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
-                       [refactor-nrepl "2.0.0-SNAPSHOT"]]
+{:user {:plugins      [[cider/cider-nrepl "0.10.2"]
+                       [refactor-nrepl "2.0.0"]]
         :dependencies [[alembic "0.3.2"]
                        [org.clojure/tools.nrepl "0.2.12"]]}}
